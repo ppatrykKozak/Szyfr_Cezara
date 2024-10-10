1069d370135bfe54af069218e75b261e6cbf58e0
Patryk <Patryk@DESKTOP-7JF105N>
2024-10-10 19:38:49 +0200

    Add project files.

---
+++ Szyfr_Cezara/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+class Szyfr_Cezara
+{
+    static void Main()
+    {
+        Console.WriteLine("Witaj w programie szyfrującym i deszyfrującym Cezara!"); 
+        
+        string operacja;
+        do
+        {
+            Console.WriteLine("Wybierz operację: (S) Szyfrowanie / (D) Deszyfrowanie");
+            operacja = Console.ReadLine();
+
+            if (operacja != null)
+            {
+                operacja = operacja.Trim().ToUpper();
+            }
+            if (operacja != "S" && operacja != "D")
+            {
+                Console.WriteLine("Niewłaściwy wybór. Spróbuj ponownie, wybierając 'S' lub 'D'.");
+            }
+        }
+        while (operacja != "S" && operacja != "D");
+    
+        Console.WriteLine("Wprowadź tekst do przetworzenia:");
+        string tekst = Console.ReadLine();
+   
+        int przesuniecie;
+        do
+        {
+            Console.WriteLine("Podaj wartość przesunięcia (np. 3 dla szyfrowania o 3 miejsca):");
+            if (!int.TryParse(Console.ReadLine(), out przesuniecie))
+            {
+                Console.WriteLine("Nieprawidłowa wartość. Podaj liczbę całkowitą.");
+            }
+            else
+            {
+                break; 
+            }
+
+        }
+        while (true);
+   
+        int finalnePrzesuniecie;
+        if (operacja == "D")
+        {
+            finalnePrzesuniecie = -przesuniecie; 
+        }
+        else
+        {
+            finalnePrzesuniecie = przesuniecie;
+        }
+
+        string wynik = PrzetworzTekst(tekst, finalnePrzesuniecie);
+        Console.WriteLine($"Wynik przetworzenia: {wynik}");
+    }
+    
+    static string PrzetworzTekst(string wejscie, int przesuniecie)
+    {
+        char[] znakiTekstu = new char[wejscie.Length];
+
+        for (int i = 0; i < wejscie.Length; i++)
+        {
+            char znak = wejscie[i];
+            if (char.IsLetter(znak))
+            {
+                char baza;
+                if (char.IsUpper(znak))
+                {
+                    baza = 'A';
+                }
+                else
+                {
+                    baza = 'a';
+                }
+                znak = (char)((znak - baza + przesuniecie + 26) % 26 + baza);
+            }
+            znakiTekstu[i] = znak;
+        }
+        return new string(znakiTekstu);
+    }
+}
+ 
